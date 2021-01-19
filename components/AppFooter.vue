4b23fd1b014b8abd96aeff69d54b4b809038f44e
Elliot Matthews <elliotmatthews101@gmail.com>
2021-01-19 14:45:32 +0000

    Committing first version

---
+++ components/AppFooter.vue
@@ -0,0 +1,10 @@
+<template>
+  <footer>Copyright Vegan Legends</footer>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <div class="content">
+      <AppMenu />
+      <Nuxt />
+    </div>
+    <AppFooter />
+  </div>
+</template>
+
+<style>
+html {
+  font-family: "Gloria Hallelujah", -apple-system, BlinkMacSystemFont,
+    "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
+  font-size: 16px;
+  word-spacing: 1px;
+  -ms-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  box-sizing: border-box;
+}
+
+*,
+*::before,
+*::after {
+  box-sizing: border-box;
+  margin: 0;
+}
+
+h1,
+h2,
+h3,
+h4 {
+  font-family: "Permanent Marker", -apple-system, BlinkMacSystemFont, "Segoe UI",
+    Roboto, "Helvetica Neue", Arial, sans-serif;
+}
+</style>
+<script>
+import AppMenu from "@/components/AppMenu.vue";
+import AppFooter from "@/components/AppFooter.vue";
+
+export default {
+  components: {
+    AppMenu,
+    AppFooter,
+  },
+};
+</script>
